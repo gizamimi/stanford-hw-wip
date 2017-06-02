1e93f615a8eab9cb11021c6a7d3c2615df7e550b
gizamimi <gizamimi@users.noreply.github.com>
2017-06-02 13:21:17 +0800

    Update Pyramid.java

--- hw2/Pyramid.java
+++ hw2/Pyramid.java
@@ -26,16 +26,18 @@ public class Pyramid extends GraphicsProgram {
 	private static final int BRICKS_IN_BASE = 14;
 	
 	public void run() {
-		for (int row = 0; row < BRICKS_IN_BASE; row++) {
+		//define vertically: number of rows
+		for (int row = 0; row < BRICKS_IN_BASE; row++) { 
 			int bricksInRow = BRICKS_IN_BASE - row; 
-			/*row 0(last row) has 14 bricksInRow. 
-			 * row 1 has 13 bricks, row 2 has 12 bricks...top row 13 has 14 bricks
+			/* row 0(last row) has 14 bricksInRow. 
+			 * from bottom to top: row 1 has 13 bricks, row 2 has 12 bricks...top row 13 has 14 bricks
 			 * thus bricksInRow is BRICKS_IN_BASE(14) - row.
 			 */
-			for (int brickNum= 0; brickNum < bricksInRow; brickNum++){
-				int x = getWidth()/2 - row*BRICK_WIDTH/2 + brickNum * BRICK_WIDTH;
+			//define horizontally: number of bricks per row
+			for (int brickNum = 0; brickNum < bricksInRow; brickNum++){
+				int x = getWidth()/2 - bricksInRow * BRICK_WIDTH / 2 + brickNum * BRICK_WIDTH;
 				//brickNum starts from 0. the first brick's x starts from 0
-				int y = getHeight() - (row+1)*BRICK_HEIGHT;
+				int y = getHeight() - (row+1) * BRICK_HEIGHT;
 				//row
 				GRect brick = new GRect(x,y,BRICK_WIDTH,BRICK_HEIGHT);
 				add(brick);
