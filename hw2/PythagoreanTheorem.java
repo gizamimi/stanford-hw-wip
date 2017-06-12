3438c4f6fcef6e2c227e02084c5e231a8b767b44
gizamimi <gizamimi@users.noreply.github.com>
2017-06-12 15:26:00 +0800

    Create PythagoreanTheorem.java

---
+++ hw2/PythagoreanTheorem.java
@@ -0,0 +1,21 @@
+/*
+ * File: PythagoreanTheorem.java
+ * Name: 
+ * Section Leader: 
+ * -----------------------------
+ * This file is the starter file for the PythagoreanTheorem problem.
+ */
+
+import acm.program.*;
+
+public class PythagoreanTheorem extends ConsoleProgram {
+	public void run() {
+		println("Enter values to computer Pythagorean theorem.");
+		int a = readInt("a: ");
+		int b = readInt("b: ");
+		double c = readDouble("c = " + Math.sqrt(a*a + b*b));
+		println(a);
+		println(b);
+		println(c);
+	}
+}
