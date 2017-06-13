2c5a16d4c1a683bfa4dba76251cf4b18b957a98e
gizamimi <gizamimi@users.noreply.github.com>
2017-06-13 11:02:29 +0800

    Create FindRange.java

--- hw2/FindRange.java
+++ hw2/FindRange.java
@@ -1,1 +1,37 @@
+/*
+ * File: FindRange.java
+ * Name: 
+ * Section Leader: 
+ * --------------------
+ * This file is the starter file for the FindRange problem.
+ */
 
+import acm.program.*;
+
+public class FindRange extends ConsoleProgram {
+	public void run() {
+		println("This program finds the largest and smallest numbers.");
+		int firstInput = readInt("? ");
+		
+		if (firstInput == sentinel){
+			println("Not valid first number!");
+		}
+		int smallest = firstInput;
+		int largest = firstInput;
+		
+		while (firstInput != sentinel){
+			int nextInput = readInt("? ");
+			if (nextInput == sentinel){ //inside while loop, must end in this condition (when press 0) //
+				println ("smallest: " + smallest);
+				println ("largest: " + largest);
+				firstInput = sentinel; //break while loop, println//
+			} else if (nextInput < smallest){ 
+				smallest = nextInput; //return to while loop//
+			} else if (nextInput>largest){
+				largest = nextInput; //return to while loop//
+			}
+		}
+		
+	}
+	private static final int sentinel = 0;
+}
